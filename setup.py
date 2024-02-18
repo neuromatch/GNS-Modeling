1143e68e744e66c732a6ee9e0aef346350ffd766
Samuele <bolottasamuele@gmail.com>
2024-02-18 16:58:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,4 @@ from setuptools import find_packages, setup
 setup(
     name='gns',
     packages=find_packages(),
-    package_data={
-        'gns': ['model_saves/*.pt'],
-    },
-    include_package_data=True,
 )
